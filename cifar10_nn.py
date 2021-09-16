d7518c7638d8ecdce75f9c9b1045b54e96467a4d
lewiis252 <krystianoleniacz@gmail.com>
2021-09-16 10:04:22 +0200

    Changed to distinguish beetwen birds and airplanes.

--- cifar10_nn.py
+++ cifar10_nn.py
@@ -46,20 +46,36 @@ cifar10_val = datasets.CIFAR10(
         transforms.Normalize((0.4915, 0.4823, 0.4468),
                              (0.2470, 0.2435, 0.2616))]))
 
+# comment this and change output neurons (and dataloader far below) if you want only to find difference beetwenn planes and birds
+label_map = {0: 0, 2: 1}
+class_names = ['airplane', 'bird']
+cifar10_train_ = [(img, label_map[label])
+          for img, label in cifar10_train
+          if label in [0, 2]]
+cifar10_val_ = [(img, label_map[label])
+              for img, label in cifar10_val
+              if label in [0, 2]]
+
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.act1 = nn.Tanh()
+        self.pool1 = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
+        self.act2 = nn.Tanh()
+        self.pool2 = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(8 * 8 * 8, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.act3 = nn.Tanh()
+        self.fc2 = nn.Linear(32, 2)
+
 
     def forward(self, x):
-        out = F.max_pool2d(torch.tanh(self.conv1(x)), 2)
-        out = F.max_pool2d(torch.tanh(self.conv2(out)), 2)
-        out = out.view(-1, 8 * 8 * 8)
-        out = torch.tanh(self.fc1(out))
+        out = self.pool1(self.act1(self.conv1(x)))
+        out = self.pool2(self.act2(self.conv2(out)))
+        out = out.view(-1, 8 * 8 * 8)  # <1>
+        out = self.act3(self.fc1(out))
         out = self.fc2(out)
         return out
 
@@ -85,10 +101,10 @@ def training_loop(n_epochs, optimizer, model, loss_fn, train_loader):
 
             loss_train += loss.item()
 
-        if epoch == 1 or epoch % 10 == 0:
-            print('{} Epoch {}, Training loss {}'.format(
-                datetime.datetime.now(), epoch,
-                loss_train / len(train_loader)))
+        # if epoch == 1 or epoch % 10 == 0:
+        print('{} Epoch {}, Training loss {}'.format(
+            datetime.datetime.now(), epoch,
+            loss_train / len(train_loader)))
 
 def validate(model, train_loader, val_loader):
     for name, loader in [("train", train_loader), ("val", val_loader)]:
@@ -106,17 +122,12 @@ def validate(model, train_loader, val_loader):
 
         print("Accuracy {}: {:.2f}".format(name , correct / total))
 
-
-
-
-
-
 n_epochs = 100
 model = Net().to(device=device)
-optimizer = optim.SGD(model.parameters(), lr=1e-2)
+optimizer = optim.Adam(model.parameters(), lr=1e-2)
 loss_fn = nn.CrossEntropyLoss()
-train_loader = torch.utils.data.DataLoader(cifar10_train, batch_size=64, shuffle=False)
-val_loader = torch.utils.data.DataLoader(cifar10_val, batch_size=64, shuffle=False)
+train_loader = torch.utils.data.DataLoader(cifar10_train_, batch_size=64, shuffle=False)
+val_loader = torch.utils.data.DataLoader(cifar10_val_, batch_size=64, shuffle=False)
 
 training_loop(
     n_epochs = n_epochs,
